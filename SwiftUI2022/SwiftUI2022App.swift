d2eabad2bf3bb6948759f8ab1454421a5f14b442
Starsky <starsky.m@live.com>
2022-03-11 18:50:47 +0100

    Initial Commit

---
+++ SwiftUI2022/SwiftUI2022App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2022App.swift
+//  SwiftUI2022
+//
+//  Created by star on 11.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
